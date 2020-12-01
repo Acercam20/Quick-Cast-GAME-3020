c354af224f7c242ad7aec0783e77c54f2b4eb479
Acercam20 <cameronakey@hotmail.ca>
2020-12-01 15:07:05 -0500

    Spawning bug fix and code condesing
    Fixed part of an issue where object spawn inside of walls, as well as condensed some code for the spawning

--- Assets/_Scripts/World Scripts/KeyBehaviour.cs
+++ Assets/_Scripts/World Scripts/KeyBehaviour.cs
@@ -7,19 +7,7 @@ public class KeyBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int decider = Random.Range(1, 3);
-        if (decider == 1)
-        {
-            gameObject.transform.position = new Vector3(-25, 10, 0);
-        }
-        else if (decider == 2)
-        {
-            gameObject.transform.position = new Vector3(-15, 10, 0);
-        }
-        else
-        {
-            gameObject.transform.position = new Vector3(-20, 10, 0);
-        }
+      
     }
 
     // Update is called once per frame
--- Assets/_Scripts/World Scripts/LevelGeneration.cs
+++ Assets/_Scripts/World Scripts/LevelGeneration.cs
@@ -7,13 +7,18 @@ public class LevelGeneration : MonoBehaviour
 
     public Transform[] startingPositions;
     public GameObject[] rooms;
+
+    public GameObject keyPrefab;
+    public GameObject doorPrefab;
+    public int doorSafetyRadius;
+
     private GameObject[] wallArray;
     private GameObject[] boundaryArray;
 
     public bool[,] isWallThere;
     private int arrayHeight = 42;
     private int arrayWidth = 42;
-
+    private bool keyIsSpawned = false;
     //private int direction;
 
     private void Start()
@@ -28,37 +33,56 @@ public class LevelGeneration : MonoBehaviour
         }
 
         isWallThere = new bool[arrayWidth, arrayHeight];
-        Debug.Log(isWallThere[40,40]);
-
-        wallArray = GameObject.FindGameObjectsWithTag("WallPrefab");
         boundaryArray = GameObject.FindGameObjectsWithTag("BoundaryBlock");
-
-        for (int i = 0; i < wallArray.Length; i++)
-        {
-            isWallThere[Mathf.Abs((int)wallArray[i].transform.position.x), (int)wallArray[i].transform.position.y] = true;
-        }
+        
         for (int i = 0; i < boundaryArray.Length; i++)
         {
-            Debug.Log("I: " + i);
-            Debug.Log("Boundary Length: " + boundaryArray.Length);
-            Debug.Log("isWallThereLength: " + isWallThere.Length / arrayHeight);
-            Debug.Log("Array location: " + Mathf.Abs((int)boundaryArray[i].transform.position.x) + ", " + Mathf.Abs((int)boundaryArray[i].transform.position.y));
             isWallThere[Mathf.Abs((int)boundaryArray[i].transform.position.x), (int)boundaryArray[i].transform.position.y] = true;
-            //isWallThere[20, (int)boundaryArray[i].transform.position.y] = true;
         }
 
+        Debug.Log(isWallThere[0, 0]);
         //direction = Random.Range(1, 6); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!keyIsSpawned)
+        {
+            keyIsSpawned = true;
+            wallArray = GameObject.FindGameObjectsWithTag("WallPrefab");
+
+            for (int i = 0; i < wallArray.Length; i++)
+            {
+                isWallThere[Mathf.Abs((int)wallArray[i].transform.position.x), (int)wallArray[i].transform.position.y] = true;
+            }
+            SpawnDoor();
+            SpawnKey();
+        }
     }
 
     //StartCoroutine(WaitForLoad(1.0f));
     IEnumerator WaitForLoad(float time)
     {
         yield return new WaitForSeconds(time);
+    }
+
+    void SpawnKey()
+    {
+        int spawnX = Random.Range(0, arrayWidth);
+        int spawnY = Random.Range(0, arrayHeight);
+        while (isWallThere[spawnX, spawnY])
+        {
+            spawnX = Random.Range(0, arrayWidth);
+            spawnY = Random.Range(0, arrayHeight);
+        }
+        Instantiate(keyPrefab, new Vector3(-spawnX, spawnY, 0), Quaternion.identity);
+    }
+
+    void SpawnDoor()
+    {
+        int doorX = -20;
+        int doorY = 25;
+        Instantiate(doorPrefab, new Vector3(doorX, doorY, 0), Quaternion.identity);
     }
 }
